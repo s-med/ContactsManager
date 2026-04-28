95e0ad79b1cc040600b1bd7c491ef1171b7fe937
Sidibe Mohamed <sidibemohamed0753@gmail.com>
2026-04-28 09:08:26 +0000

    feat: implémentation de la classe Contact

---
+++ src/main/java/Contact.java
@@ -0,0 +1,9 @@
+public class Contact {
+    String nom;
+    String numeroTelephone;
+
+    public Contact(String nom, String numeroTelephone) {
+        this.nom = nom;
+        this.numeroTelephone = numeroTelephone;
+    }
+}
